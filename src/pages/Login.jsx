a07f082807b3f977e673fd88b64a50353518ce65
Santhoshinibs <santhoshinibs5@gmail.com>
2025-06-16 15:33:45 +0530

    Added REACT_APP_API_URL

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
   const handleLogin = async (e) => {
   e.preventDefault();
   try {
-    const response = await axios.post("http://localhost:5000/api/users/login", {
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/users/login`, {
       email,
       password,
     });
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,7 +13,7 @@ function Register() {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/users/register", {
+      await axios.post(`${process.env.REACT_APP_API_URL}/users/register`, {
         name,
         email,
         password,
