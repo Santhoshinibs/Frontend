454aa6bec0d57372fcd54f41a1b68b009ad2545d
Santhoshinibs <santhoshinibs5@gmail.com>
2025-09-04 15:14:44 +0530

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,16 +5,16 @@ import { Link } from "react-router-dom";
 function ProductCard({ product }) {
   return (
     <Card.Img
-  variant="top"
-  src={
-    product.image?.startsWith('http')
-      ? product.image
-      : `${import.meta.env.VITE_API_URL}${product.image}`
-  }
-  onError={(e) => { e.target.src = '/fallback.jpg'; }}
-  alt={product.name}
-  style={{ objectFit: "cover", height: "200px" }}
-/>
+        variant="top"
+        src={
+          product.image.startsWith("http")
+            ? product.image
+            : `${import.meta.env.VITE_API_URL}${product.image}`
+        }
+        onError={(e) => { e.target.src = '/fallback.jpg'; }}
+        alt={product.name}
+        style={{ objectFit: "cover", height: "200px" }}
+      />
 
       <Card.Body className="d-flex flex-column justify-content-between">
         <div>
@@ -35,6 +35,7 @@ function ProductCard({ product }) {
 }
 
 export default ProductCard;
+
 
 
 
